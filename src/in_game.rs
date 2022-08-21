1285042fac3399f16f24b06f1488e626d10e6863
Jussi <jkallio@gmail.com>
2022-08-21 17:58:59 +0300

    Add block movement with arrowkeys

--- src/in_game.rs
+++ src/in_game.rs
@@ -1,24 +1,86 @@
 use crate::prelude::*;
 use bevy::prelude::*;
 
-pub struct InGamePlugin;
+pub struct SpawnBlockEvent;
 
+#[derive(Component)]
+pub struct GameObject;
+
+#[derive(Component)]
+pub struct DroppingBlock;
+
+#[derive(Component)]
+pub struct DropSpeed(pub f32);
+
+pub struct InGamePlugin;
 impl Plugin for InGamePlugin {
     fn build(&self, app: &mut App) {
         app.add_system_set(SystemSet::on_enter(GameState::InGame).with_system(on_enter))
             .add_system_set(SystemSet::on_exit(GameState::InGame).with_system(on_exit))
             .add_system_set(
                 SystemSet::on_update(GameState::InGame).with_system(back_to_menu_on_esc),
-            );
+            )
+            .add_system_set(
+                SystemSet::on_update(GameState::InGame).with_system(update_dropping_block_movement),
+            )
+            .add_system_set(
+                SystemSet::on_update(GameState::InGame).with_system(spawn_dropping_block),
+            )
+            .add_event::<SpawnBlockEvent>();
     }
 }
 
-fn on_enter() {
+fn on_enter(mut event_writer: EventWriter<SpawnBlockEvent>) {
     println!("Enter GameState::InGame");
+    event_writer.send(SpawnBlockEvent);
 }
 
-fn on_exit() {
+fn spawn_dropping_block(mut commands: Commands, event_reader: EventReader<SpawnBlockEvent>) {
+    if !event_reader.is_empty() {
+        commands
+            .spawn_bundle(SpriteBundle {
+                sprite: Sprite {
+                    color: Color::BLACK,
+                    custom_size: Some(Vec2::new(BLOCK_SIZE, BLOCK_SIZE)),
+                    ..Default::default()
+                },
+                ..Default::default()
+            })
+            .insert(DroppingBlock)
+            .insert(GameObject)
+            .insert(DropSpeed(-50.0));
+    }
+}
+
+fn on_exit(mut commands: Commands, query: Query<Entity, With<GameObject>>) {
     println!("Exit GameState::InGame");
+
+    for entity in query.iter() {
+        commands.entity(entity).despawn_recursive();
+    }
+}
+
+fn update_dropping_block_movement(
+    time: Res<Time>,
+    input: Res<Input<KeyCode>>,
+    mut query: Query<(&mut Transform, &DropSpeed), With<DroppingBlock>>,
+) {
+    for (mut transform, drop_speed) in query.iter_mut() {
+        if input.just_pressed(KeyCode::Left) || input.just_pressed(KeyCode::A) {
+            transform.translation.x -= BLOCK_SIZE;
+        } else if input.just_pressed(KeyCode::Right) || input.just_pressed(KeyCode::A) {
+            transform.translation.x += BLOCK_SIZE;
+        }
+
+        let drop_multiplier;
+        if input.pressed(KeyCode::Down) || input.pressed(KeyCode::S) {
+            drop_multiplier = 3.0;
+        } else {
+            drop_multiplier = 1.0;
+        }
+
+        transform.translation.y += drop_speed.0 * drop_multiplier * time.delta_seconds();
+    }
 }
 
 pub fn back_to_menu_on_esc(
--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,10 @@
 //! Idea is to create a puzzle game where player can pick any two objects to create a tool
 //! that can overcome an obstacle. E.g. combine stick with rock to create a hammer
 //!
+//! Okay... Another idea is to create a tetris-like game where number blocks drop from the sky
+//! and the goal is to get them disappear by combining them using somekind of math
+//!
 //! TODO:
-//!     - Create a Main Menu and In game States
-//!     - Create a player and move it with arrow keys
 //!     - Create a system to pick objects
 //!     - Create a two slot inventory
 //!     - Draw the game art
@@ -16,13 +17,30 @@
 //!     - Stick + Steel = Pickaxe
 
 use bevy::prelude::*;
+use bevy::render::camera::ScalingMode;
 use in_game::InGamePlugin;
 use menu::MenuPlugin;
+mod collider;
 mod in_game;
 mod menu;
 
+/// Window aspect ratio
+const ASPECT_RATIO: f32 = 8.0 / 10.0;
+
+/// Pixel height of the game world
+const WORLD_HEIGHT: f32 = 240.0;
+
+/// Window scale factor
+const WINDOW_SCALE: f32 = 3.0;
+
+/// Size of the game blocks
+pub const BLOCK_SIZE: f32 = 64.;
+
+/// Resource for holding the window size
+pub struct WindowSize(Vec2);
+
 pub mod prelude {
-    pub use super::{GameCamera, GameState};
+    pub use super::{GameCamera, GameState, BLOCK_SIZE};
 }
 
 pub struct LaunchMenu;
@@ -38,14 +56,18 @@ pub enum GameState {
 pub struct GameCamera;
 
 fn main() {
+    let win_size = Vec2::new(WORLD_HEIGHT * ASPECT_RATIO, WORLD_HEIGHT);
+
     App::new()
         .insert_resource(WindowDescriptor {
-            width: 800.0,
-            height: 600.0,
-            resizable: false,
+            title: "Combine".into(),
+            width: win_size.x * WINDOW_SCALE,
+            height: win_size.y * WINDOW_SCALE,
+            resizable: true,
             ..Default::default()
         })
         .insert_resource(ClearColor(Color::BLUE))
+        .insert_resource(WindowSize(win_size))
         .add_event::<LaunchMenu>()
         .add_state(GameState::Init)
         .add_plugins(DefaultPlugins)
@@ -57,9 +79,20 @@ fn main() {
 }
 
 pub fn game_setup(mut commands: Commands, mut launch_event: EventWriter<LaunchMenu>) {
-    commands
-        .spawn_bundle(Camera2dBundle::default())
-        .insert(GameCamera);
+    let mut camera = Camera2dBundle::default();
+
+    // No re-scaling on windows resize
+    camera.projection.scaling_mode = ScalingMode::None;
+    camera.projection.scale = 600.0;
+
+    // Fix the aspect ratio
+    camera.projection.top = 1.0;
+    camera.projection.bottom = -1.0;
+    camera.projection.left = -1.0 * ASPECT_RATIO;
+    camera.projection.right = 1.0 * ASPECT_RATIO;
+
+    // Spawn the camera
+    commands.spawn_bundle(camera).insert(GameCamera);
 
     launch_event.send(LaunchMenu);
 }
